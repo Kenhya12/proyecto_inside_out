ba1fdf1c30280c965aa886db54d8b30c7929bc88
Paula Apsé <89433554+Kenhya12@users.noreply.github.com>
2025-08-22 12:17:15 +0200

    test: corrección test MomentoDTOTest

--- src/test/java/com/insideout/dto/MomentoDTOTest.java
+++ src/test/java/com/insideout/dto/MomentoDTOTest.java
@@ -6,41 +6,38 @@ import static org.junit.jupiter.api.Assertions.*;
 class MomentoDTOTest {
 
     @Test
-    void testConstructorVacio() {
-        MomentoDTO dto = new MomentoDTO();
-        assertNotNull(dto);
-    }
-
-    @Test
     void testConstructorConArgumentos() {
-        MomentoDTO dto = new MomentoDTO(1, "Título", "Descripción", "21/08/2025", "ALEGRIA");
+        MomentoDTO dto = new MomentoDTO(1, "Título", "Descripción", "21/08/2025", "ALEGRIA", "BUENO");
         assertEquals(1, dto.getId());
         assertEquals("Título", dto.getTitulo());
         assertEquals("Descripción", dto.getDescripcion());
         assertEquals("21/08/2025", dto.getFecha());
         assertEquals("ALEGRIA", dto.getEmocion());
+        assertEquals("BUENO", dto.getSentimiento());
     }
 
     @Test
     void testGettersAndSetters() {
-        MomentoDTO dto = new MomentoDTO();
+        MomentoDTO dto = new MomentoDTO(0, "", "", "", "", ""); // Se utiliza un constructor válido para inicializar
         dto.setId(2);
         dto.setTitulo("Título 2");
         dto.setDescripcion("Descripción 2");
         dto.setFecha("22/08/2025");
         dto.setEmocion("TRISTEZA");
+        dto.setSentimiento("MALO");
 
         assertEquals(2, dto.getId());
         assertEquals("Título 2", dto.getTitulo());
         assertEquals("Descripción 2", dto.getDescripcion());
         assertEquals("22/08/2025", dto.getFecha());
         assertEquals("TRISTEZA", dto.getEmocion());
+        assertEquals("MALO", dto.getSentimiento());
     }
 
     @Test
     void testToString() {
-        MomentoDTO dto = new MomentoDTO(3, "Título 3", "Descripción 3", "23/08/2025", "IRA");
-        String expectedString = "ID: 3, Título: Título 3, Emoción: IRA, Fecha: 23/08/2025, Descripción: Descripción 3";
+        MomentoDTO dto = new MomentoDTO(3, "Título 3", "Descripción 3", "23/08/2025", "IRA", "MALO");
+        String expectedString = "ID: 3, Título: Título 3, Emoción: IRA, Sentimiento: MALO, Fecha: 23/08/2025, Descripción: Descripción 3";
         assertEquals(expectedString, dto.toString());
     }
 }
